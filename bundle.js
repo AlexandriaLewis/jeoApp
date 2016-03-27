2768198947eb669261c98e15ac72983a71ee8f45
lexria <alexandriamlewis@me.com>
2016-03-27 15:54:52 -0400

    styled

--- bundle.js
+++ bundle.js
@@ -52,29 +52,20 @@ angular
         console.log('i should have 6');
         $scope.addScore = function(input, answer, val){
           console.log('clicked a btn',input);
-          var cluesArr = $scope.question.clues;
-          console.log('scope arr',cluesArr);
           console.log('answer',answer);
           if(input === answer){
             $rootScope.score += val;
           }else{
             $rootScope.score -= val;
           }
-        }
+        };
+        $scope.disableBtn = function(id){
+          $('button.'+id).prop('disabled', true);
+          $('#'+id).modal('hide');
+        };
       }
     }
   })
-  // .directive('questionBar', function(){
-  //   return {
-  //     templateUrl: '../templates/category.html',
-  //     restrict: 'E',
-  //     scope: {
-  //       answer: '='
-  //     },
-  //     controller: function($rootScope,$scope){
-  //
-  //     }
-  // })
 
 },{}],4:[function(require,module,exports){
 var angular = require('angular');
--- directives/boardDirective.js
+++ directives/boardDirective.js
@@ -12,26 +12,17 @@ angular
         console.log('i should have 6');
         $scope.addScore = function(input, answer, val){
           console.log('clicked a btn',input);
-          var cluesArr = $scope.question.clues;
-          console.log('scope arr',cluesArr);
           console.log('answer',answer);
           if(input === answer){
             $rootScope.score += val;
           }else{
             $rootScope.score -= val;
           }
-        }
+        };
+        $scope.disableBtn = function(id){
+          $('button.'+id).prop('disabled', true);
+          $('#'+id).modal('hide');
+        };
       }
     }
   })
-  // .directive('questionBar', function(){
-  //   return {
-  //     templateUrl: '../templates/category.html',
-  //     restrict: 'E',
-  //     scope: {
-  //       answer: '='
-  //     },
-  //     controller: function($rootScope,$scope){
-  //
-  //     }
-  // })
